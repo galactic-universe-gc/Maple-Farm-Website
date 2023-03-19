374980f7e41314e29ac381f35f233fafd39f7961
galactic-universe-gc <115098036+galactic-universe-gc@users.noreply.github.com>
2023-03-19 21:01:45 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,7 @@
-const isMobile = navigator.userAgentData.mobile;
-
-if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/SamsungBrowser/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
     container = document.getElementById("container");
     container.style.fontSize = "60px";
-    alert("Mobile")
 } else {
     container = document.getElementById("container");
     container.style.fontSize = "100px";
-    alert("Desktop")
 }
