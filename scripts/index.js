cf033f6cd4536eafcb6c4b85f5f479d6899bd91a
Jasper W <115098036+galactic-universe-gc@users.noreply.github.com>
2023-03-19 13:43:14 +0000

    Committed

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+container = document.getElementById("container");
+container.style.fontSize = "100px";
+
+var answer = window.orientation > 1;
+alert(answer ? 'It is a mobile device' : 'It is not a mobile device');
