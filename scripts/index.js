26404dd5c6b949ba3714b7c4d2372a1619888dce
Jasper W <115098036+galactic-universe-gc@users.noreply.github.com>
2023-03-19 13:48:17 +0000

    Committed

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 container = document.getElementById("container");
 container.style.fontSize = "100px";
 
-var answer = window.orientation > 1;
-alert(answer ? 'It is a mobile device' : 'It is not a mobile device');
+const isMobile = navigator.userAgentData.mobile;
+
+if (isMobile == True) {
+    alert("Mobile Device")
+} else {
+    alert("Not Mobile Device")
+}
